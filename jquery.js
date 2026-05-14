765d41b96961a710442fa704a2e574269091abef
Aaronkkfxz <aaronkkfx@gmail.com>
2026-05-14 00:52:53 -0300

    Finalizando

--- jquery.js
+++ jquery.js
@@ -1,118 +1,101 @@
-$(document).ready(function(){
-    carregarTarefas();
+$(document).ready(function () {
+  carregarTarefas();
+  renderizaTabela();
+
+  $("#titulo").on("input", function () {
+    $("#erro-titulo").text("");
+  });
+
+  $("#form-tarefa").on("submit", function (e) {
+    e.preventDefault();
+
+    $('#erro-titulo').text('');
+    let idEdicao = $("#tarefa-id").val();
+
+    if (idEdicao) {
+      let dadosAtualizados = {
+        titulo: $("#titulo").val(),
+        descricao: $("#descricao").val(),
+        prioridade: $("#prioridade").val(),
+        dataLimite: $("#data-limite").val(),
+        status: $("#status").val(),
+        observacao: $("#observacao").length ? $("#observacao").val() : "",
+      };
+
+      atualizarTarefa(idEdicao, dadosAtualizados);
+    } else {
+      let tarefaCriada = criaTarefa();
+
+      if (tarefaCriada == null) {
+        $("#erro-titulo").text("O título é obrigatório");
+
+        return;
+      }
+    }
+
     renderizaTabela();
 
-    $('#titulo').on('input', function(){
+    this.reset();
+    $('#btn-concluir').text('Concluir Tarefa');
+    $("#tarefa-id").val("");
+    $("#grupo-observacao").remove();
+  });
 
-        $('#erro-titulo').text('');
-
-    });
-
-
-    $('#form-tarefa').on('submit', function(e){
-        e.preventDefault();
-
-        let idEdicao = $('#tarefa-id').val();
-
-        if(idEdicao){
-
-            let dadosAtualizados = {
-
-                titulo : $('#titulo').val(),
-                descricao : $('#descricao').val(),
-                prioridade : $('#prioridade').val(),
-                dataLimite : $('#data-limite').val(),
-                status : $('#status').val(),
-                observacao : $('#observacao').length ? $('#observacao').val() : ''
-
-            };
-
-            atualizarTarefa(idEdicao, dadosAtualizados);
-
-        }
-        else{
-
-            let tarefaCriada = criaTarefa();
-
-            if(tarefaCriada == null){
-
-                $('#erro-titulo').text('O título é obrigatório');
-
-                return;
-
-            }
-
-        }
-
-        renderizaTabela();
-
-        this.reset();
-        $('#tarefa-id').val('');
-        $('#grupo-observacao').remove();
-    });
-
-    $('#btn-observacao').on('click', function(){
-        if($('#observacao').length === 0){
-            $('#area-observacao').append(`
+  $("#btn-observacao").on("click", function () {
+    if ($("#observacao").length === 0) {
+      $("#area-observacao").append(`
                 <div class="field-group" id="grupo-observacao">
                     <label for="observacao">Observação</label>
                     <textarea id="observacao" rows="3" placeholder="Digite a observação..."></textarea>
                 </div>
             `);
-        }
-        else{
-            $('#grupo-observacao').remove();
-        }
+    } else {
+      $("#grupo-observacao").remove();
+    }
+  });
+
+  $(document).on("click", ".btn-delete", function () {
+    let id = $(this).data("id");
+    excluirTarefa(id);
+    renderizaTabela();
+  });
+
+  $("#btn-filtrar").on("click", function () {
+    let status = $("#filtro-status").val();
+    let prioridade = $("#filtro-prioridade").val();
+
+    let tarefasFiltradas = filtrarTarefas(status, prioridade);
+
+    renderizaTabela(tarefasFiltradas);
+  });
+
+  $("#btn-limpar-filtro").on("click", function () {
+    $("#filtro-status").val("");
+    $("#filtro-prioridade").val("");
+
+    renderizaTabela();
+  });
+
+  $(document).on("click", ".btn-edit", function () {
+    let id = $(this).data("id");
+
+    let tarefaSelecionada = tarefa.find(function (item) {
+      return item.Id == id;
     });
 
-    $(document).on('click', '.btn-delete', function(){
-        let id = $(this).data('id');
-        excluirTarefa(id);
-        renderizaTabela();
-    });
+    $("#titulo").val(tarefaSelecionada.titulo);
+    $("#descricao").val(tarefaSelecionada.descricao);
+    $("#prioridade").val(tarefaSelecionada.prioridade);
+    $("#data-limite").val(tarefaSelecionada.dataLimite);
+    $("#status").val(tarefaSelecionada.status);
 
-    $('#btn-filtrar').on('click', function(){
+    $("#tarefa-id").val(tarefaSelecionada.Id);
 
-        let status = $('#filtro-status').val();
-        let prioridade = $('#filtro-prioridade').val();
+    $('#btn-concluir').text('Atualizar Tarefa');
 
-        let tarefasFiltradas = filtrarTarefas(status, prioridade);
-
-        renderizaTabela(tarefasFiltradas);
-
-    });
-
-    $('#btn-limpar-filtro').on('click', function(){
-
-        $('#filtro-status').val('');
-        $('#filtro-prioridade').val('');
-
-        renderizaTabela();
-
-    });
-
-    $(document).on('click', '.btn-edit', function(){
-
-        let id = $(this).data('id');
-
-        let tarefaSelecionada = tarefa.find(function(item){
-            return item.Id == id;
-        });
-
-        $('#titulo').val(tarefaSelecionada.titulo);
-        $('#descricao').val(tarefaSelecionada.descricao);
-        $('#prioridade').val(tarefaSelecionada.prioridade);
-        $('#data-limite').val(tarefaSelecionada.dataLimite);
-        $('#status').val(tarefaSelecionada.status);
-        
-
-        $('#tarefa-id').val(tarefaSelecionada.Id);
-
-        if(tarefaSelecionada.observacao){
-
-            if($('#observacao').length === 0){
-
-                $('#area-observacao').append(`
+    if (tarefaSelecionada.observacao) {
+      if ($("#observacao").length === 0) {
+        $("#area-observacao").append(`
                 
                     <div class="field-group" id="grupo-observacao">
 
@@ -127,28 +110,27 @@ $(document).ready(function(){
                     </div>
                 
                 `);
+      }
 
-            }
+      $("#observacao").val(tarefaSelecionada.observacao);
+    }
+  });
 
-            $('#observacao').val(tarefaSelecionada.observacao);
+  $(document).on("dblclick", "#tabela-tarefas tbody tr", function () {
+    let id = $(this).data("id");
 
-        }
-    });
+    $('.btn-edit[data-id="' + id + '"]').click();
+  });
+});
 
-    $(document).on('dblclick', '#tabela-tarefas tbody tr', function(){
+function renderizaTabela(lista = tarefa) {
+  if (lista.length === 0) {
+    $("#container-tabela").html("");
 
-        let id = $(this).data('id');
-
-        $('.btn-edit[data-id="' + id + '"]').click();
-
-    });
-
-    });
-
-    function renderizaTabela(lista = tarefa){
-        if($('#tabela-tarefas').length === 0){
-
-            $('#container-tabela').append(`
+    return;
+  }
+  if ($("#tabela-tarefas").length === 0) {
+    $("#container-tabela").append(`
             
                 <div class="table-wrapper">
 
@@ -176,13 +158,12 @@ $(document).ready(function(){
                 </div>
             
             `);
-        }
+  }
 
-        $('#tabela-tarefas tbody').html('');
+  $("#tabela-tarefas tbody").html("");
 
-        lista.forEach(function(item){
-
-            $('#tabela-tarefas tbody').append(`
+  lista.forEach(function (item) {
+    $("#tabela-tarefas tbody").append(`
             
                 <tr data-id="${item.Id}">
 
@@ -206,6 +187,5 @@ $(document).ready(function(){
                 </tr>
             
             `);
-
-        });
+  });
 }
